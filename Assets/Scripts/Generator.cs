1b2799c0528b647500d5a0701a9753f293069ae9
wrathchild14 <jovanprodanov123@gmail.com>
2022-12-27 18:08:10 +0100

    add all sound wave, first rough implementation of the generator

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -5,24 +5,80 @@ public class Generator : MonoBehaviour
 {
     public AudioSource audioSource;
 
-    public float frequency = 440;
+    public float startFrequency = 440;
+    public float cutoffFrequency = 880;
+    public float slideRate = 1;
+    public float vibratoFrequency = 5;
+    public float vibratoDepth = 1;
     public float volume = 1;
-    public float duration = 0.1f;
+    public float attackTime = 0.1f;
+    public float decayTime = 0.1f;
+    public float sustainLevel = 0.5f;
+    public float releaseTime = 0.1f;
+    public float arpeggioFrequency = 4;
+    public int arpeggioOctaveRange = 2;
+    
+    public float noiseFrequency = 10;
+    
+    private bool squareWave = false;
+    private bool sawtoothWave = false;
+    private bool noiseWave = true;
 
     public void CreateJumpingSound()
     {
-        const int sampleRate = 44100;
-        var numSamples = (int)(sampleRate * duration);
-        var samples = new float[numSamples];
-        for (var i = 0; i < numSamples; i++)
+        int sampleRate = 44100;
+        float duration = attackTime + decayTime + releaseTime;
+        int numSamples = (int)(sampleRate * duration);
+        float[] samples = new float[numSamples];
+        for (int i = 0; i < numSamples; i++)
         {
             float t = i / (float)sampleRate;
-            samples[i] = Mathf.Sin(2 * Mathf.PI * frequency * t) * Mathf.Pow(1 - t / duration, 3) * volume;
+            float envelope = 1;
+            if (t < attackTime)
+            {
+                envelope = t / attackTime;
+            }
+            else if (t < attackTime + decayTime)
+            {
+                envelope = 1 - (t - attackTime) / decayTime * (1 - sustainLevel);
+            }
+            else
+            {
+                envelope = sustainLevel * (1 - (t - attackTime - decayTime) / releaseTime);
+            }
+
+            float frequency = startFrequency + (cutoffFrequency - startFrequency) * t / duration * slideRate;
+            float vibrato = Mathf.Sin(2 * Mathf.PI * vibratoFrequency * t) * vibratoDepth;
+            float arpeggio = Mathf.Pow(2,
+                (Mathf.Sin(2 * Mathf.PI * arpeggioFrequency * t) * arpeggioOctaveRange + arpeggioOctaveRange) / 12) - 1;
+            
+
+            float phase = (frequency * arpeggio + vibrato) * t * 2 * Mathf.PI;
+            // todo: make switch-case
+            if (squareWave)
+            {
+                samples[i] = Mathf.Sign(Mathf.Sin(phase)) * envelope * volume;
+            }
+            else if (sawtoothWave)
+            {
+                samples[i] = (2 * (phase % (2 * Mathf.PI)) / (2 * Mathf.PI) - 1) * envelope * volume;
+            }
+            else if (noiseWave)
+            {
+                float noise = Mathf.PerlinNoise(t * noiseFrequency, 0) * 2 - 1;
+                samples[i] = noise * envelope * volume;
+            }
+            // sine wave 
+            else
+            {
+                samples[i] = Mathf.Sin(phase) * envelope * volume;
+            }
         }
 
-        var clip = AudioClip.Create("Jump Sound", numSamples, 1, sampleRate, false, false);
+        AudioClip clip = AudioClip.Create("Jump Sound", numSamples, 1, sampleRate, false, false);
         clip.SetData(samples, 0);
 
+        AudioSource audioSource = GetComponent<AudioSource>();
         audioSource.clip = clip;
     }
 
